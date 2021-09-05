0da0d4df8d716cfaf08cb005e75dfc2c47bd22da
Storm Jones <9743815+StormJones@users.noreply.github.com>
2021-09-05 13:24:37 +1000

    Switched to WeatherAPI
    OpenWeather had a constant issue where the API key would sometimes drop, so I've switched provider to something hopefully more reliable.

--- weather_api_1.php
+++ weather_api_1.php
@@ -1,13 +1,14 @@
 <?php
 	
 	// Static API Key for OpenWeather API
-	$APIKEY = "47eedbe51652e1f27b7337a28f386f22";
+	$APIKEY = "85a2b9dc008241e1a6131919210509";
 	
 	// Search Location
 	$location = "Townsville";
 	
 	// Constructed API URL
-	$weatherURL = "api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey";
+	//$weatherURL = "api.openweathermap.org/data/2.5/weather?q=$location&appid=$apiKey";
+	$weatherURL = "http://api.weatherapi.com/v1/current.json?key=$APIKEY&q=$location&aqi=no";
 	
 	// Create & initialize a curl session
 	$curl = curl_init();
@@ -27,18 +28,16 @@
 
 ?>
 
-
-
 <html>
 	<head>
 		<title>API Test One</title>
 	</head>
 	<body>
 		<h1>API Test One</h1>
-		<h2>Using OpenWeather API</h2>
+		<h2>Using <a href='https://www.weatherapi.com/' target='_blank'>Weatherapi API</a></h2>
 		<hr>
 		<b>Using API Key: </b><u><?php echo $APIKEY; ?></u><br /><br />
 		<b>Output:</b><br />
-		<?php echo $output; ?>
+		<?php print_r($output); ?>
 	</body>
 </html>
