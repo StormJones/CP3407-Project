42d53def4ca0434cbcd65a78bc18422fae919edf
Stephen Yamashita <jamal.yamashita@my.jcu.edu.au>
2021-09-05 15:52:19 +1000

    Update weather_api_2.php
    Cleaned up code a bit and made a quick display.

--- weather_api_2.php
+++ weather_api_2.php
@@ -1,10 +1,10 @@
 <?php
 $apikey ="QFIlKf2cy3y9GM3EGnGddJmNLpGBl3vG";
-$forecast_content = fopen("http://dataservice.accuweather.com/forecasts/v1/daily/1day/3494540?apikey=$apikey&language=en-us&metric=true", "r");
+
+$URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/3494540?apikey=$apikey&language=en-us&metric=true";
+$forecast_content = fopen($URL, "r");
 $daily_forecasts = stream_get_contents($forecast_content);
 fclose($forecast_content);
-$dayForecast = json_decode($daily_forecasts, true);
-print $dayForecast;
 ?>
 
 <html lang="en">
@@ -12,7 +12,14 @@ print $dayForecast;
     <meta charset="utf-8"/>
     <title>testSite</title>
 </head>
-<body>
+<>
+    <h1>API TestSite 2</h1>
+    <h2> <a href="https://developer.accuweather.com/">Accuweather API</a></h2>
+    <b>API Key: </b><u><?php print $apikey; ?> </u>
+    <br>
+    <b>Output: </b>
+    <br>
+    <?php print $daily_forecasts;?>
 
 
 </body>
